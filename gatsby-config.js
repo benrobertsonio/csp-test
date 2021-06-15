67c6b38bfd5c5b188f9f83b008ed1e7a48b404a3
Ben Robertson <hi@benrobertson.io>
2021-06-15 16:52:00 -0400

    Test raw csp.

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
         options: {
             headers: {
                 "/*": [
-                    `Content-Security-Policy: ${process.env.CONTENT_SECURITY_POLICY}`
+                    `Content-Security-Policy: "upgrade-insecure-requests; default-src data: 'unsafe-inline' 'unsafe-eval' https:; script-src data: 'unsafe-inline' 'unsafe-eval' https: blob:; style-src data: 'unsafe-inline' https:; img-src data: https: blob:; font-src data: https:; connect-src https: wss: blob:; media-src data: https: blob:; object-src https:; child-src https: data: blob:; form-action https:; report-uri https://csp.nytimes.com/report;"`
                 ]
             }
         }
